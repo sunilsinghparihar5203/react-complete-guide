9af1cfbae9d98617647997a5316039516027342b
sunil singh parihar <sunilsinghparihar5203@gmail.com>
2023-01-02 23:25:06 +0530

    changes expense  button added

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,15 +1,31 @@
-import React from 'react'
+import React,{useState} from 'react'
 import './ExpenseItem.css';
 import ExpenseDate from './ExpenseDate';
 import ExpenseDetails from './ExpenseDetails';
 import Card from '../UI/Card';
 
 const ExpenseItem = (props)=> {
+  const [Title, setTitle] = useState(props.title)
+  const [Expence, setExpence] = useState(props.amount)
+  const [Display, setDisplay] = useState(true)
+  const ChangeTitle = () =>{
+    setTitle("Updated")
+    setDisplay(false)
+  }
+
+  const ChangeExpence = () =>{
+    setExpence(100)
+  }
+  
   
   return (
+    Display &&
     <Card className='expense-item'>
         <ExpenseDate date ={props.date} />
-        <ExpenseDetails title={props.title} amount={props.amount} location={props.location} />
+        <ExpenseDetails title={Title} amount={Expence} location={props.location} />
+        <button onClick={ChangeTitle}>Delete Expense</button>
+        
+        <button onClick={ChangeExpence}>Change Expense</button>
     </Card>
   )
 }
